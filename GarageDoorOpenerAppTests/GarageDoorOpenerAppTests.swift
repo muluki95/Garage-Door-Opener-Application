ea70dce9fc93135471091ec22432ffc7eee1b716
Esther <mulukiesther@gmail.com>
2025-05-02 12:41:16 -0500

    save

---
+++ GarageDoorOpenerAppTests/GarageDoorOpenerAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  GarageDoorOpenerAppTests.swift
+//  GarageDoorOpenerAppTests
+//
+//  Created by Esther Nzomo on 5/2/25.
+//
+
+import Testing
+
+struct GarageDoorOpenerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
