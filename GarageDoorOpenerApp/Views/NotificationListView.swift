c32f3aed16d6ab2c650aafc0fca4959447cadede
Esther <mulukiesther@gmail.com>
2025-05-09 17:49:23 -0500

    commit

--- GarageDoorOpenerApp/Views/NotificationListView.swift
+++ GarageDoorOpenerApp/Views/NotificationListView.swift
@@ -12,7 +12,7 @@ struct NotificationList:View{
     
     @ObservedObject var notificationsViewModel:NotificationsViewModel
     @State private var searchText = ""
-    
+    @State private var  hasStartedLoading = false
     
     //filtered notifications based on search
     var filteredNotifications:[Notification]{
@@ -33,58 +33,66 @@ struct NotificationList:View{
         }
         
     }
-        
-        
-        var body: some View {
-            VStack{
-                //TextField("Search Notifications", text:$searchText)
-                    //.textFieldStyle(RoundedBorderTextFieldStyle())
-                    //.padding()
-                //Handles the various loading states
-                
-                switch notificationsViewModel.notifications {
-                case.loading:
-                    ProgressView("Loading Notifications...")
-                case.error(let error):
-                    VStack{
-                        Text("Failed to load Notifications:")
-                            .font(.headline)
-                        Text(error.localizedDescription)
-                            .foregroundColor(.red)
-                        Button("Retry"){
-                            Task{
-                                await notificationsViewModel.fetchNotifications()
-                            }
-                        }
-                    }
-                    .padding()
-                    
-                case .loaded(let notifications) where notifications.isEmpty :
-                    VStack{
-                        Text("No notifications found")
-                            .foregroundColor(.gray)
-                        Button("Refresh Notifications"){
-                            Task{
-                                await notificationsViewModel.fetchNotifications()
-                            }
+    
+    
+    var body: some View {
+        VStack{
+            //TextField("Search Notifications", text:$searchText)
+            //.textFieldStyle(RoundedBorderTextFieldStyle())
+            //.padding()
+            //Handles the various loading states
+            
+            switch notificationsViewModel.notifications {
+            case.loading:
+                ProgressView("Loading Notifications...")
+                    .onAppear {
+                        if !hasStartedLoading {
+                            hasStartedLoading = true
                             
+                            Task {
+                                try await Task.sleep(nanoseconds:1_500_000_000) //there is a delay of 1.5seconds before it fetches the notifications
+                                await notificationsViewModel.fetchNotifications()
+                            }
                         }
                     }
-                case .loaded:
-                    List(filteredNotifications, id: \.timestamp) { notification in
-                        NotificationRow(notification: notification)
+            case.error(let error):
+                VStack{
+                    Text("Failed to load Notifications:")
+                        .font(.headline)
+                    Text(error.localizedDescription)
+                        .foregroundColor(.red)
+                    Button("Retry"){
+                        Task{
+                            await notificationsViewModel.fetchNotifications()
+                        }
                     }
-                    
                 }
-                   
-            }
-            .navigationTitle("Notifications")
-            .task {   //triggers the fetch automatically when the view appears
-                if case.loading = notificationsViewModel.notifications {
-                    await notificationsViewModel.fetchNotifications()
+                .padding()
+                
+            case .loaded(let notifications) where notifications.isEmpty :
+                VStack{
+                    Text("No notifications found")
+                        .foregroundColor(.gray)
+                    Button("Refresh Notifications"){
+                        Task{
+                            await notificationsViewModel.fetchNotifications()
+                        }
+                        
+                    }
                 }
+            case .loaded:
+                List(filteredNotifications, id: \.timestamp) { notification in
+                    NotificationRow(notification: notification)
+                }
+                
             }
+            
         }
         
+        .navigationTitle("Notifications")
+        
     }
+}
+    
+    
 
